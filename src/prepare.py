421f59aa9452dfd3f91dfcc3d9e7273dc3a54ff3
Tang Shangxuan <bmhtsx@gmail.com>
2025-02-11 15:51:53 +0100

    bug: specify branch and commit when prepare #19
    This modify command to specify branch and commit.

--- src/prepare.py
+++ src/prepare.py
@@ -4,23 +4,24 @@ from src.utils import run_command
 from config import TMP_DIR
 
 # This function prepares environment for build
-def prepare(repo_url, logger):
+def prepare(repo_url, branch, commit_id, logger):
 
     if os.path.exists(TMP_DIR):
         shutil.rmtree(TMP_DIR)
     os.makedirs(TMP_DIR, exist_ok=False)
 
-    clone_repo(repo_url)
+    clone_repo(repo_url, branch, commit_id)
 
     install_dependencies()
 
 
-def clone_repo(repo_url):
-    run_command(f"git clone {repo_url} .", TMP_DIR)
+def clone_repo(repo_url, branch, commit_id):
+    run_command(f"git clone -b {branch} --single-branch {repo_url} .", TMP_DIR)
+    run_command(f"git checkout {commit_id}", TMP_DIR)
 
 def install_dependencies():
     run_command("pip install -r requirements.txt", TMP_DIR)
 
 # used for test
 if __name__ == "__main__":
-    prepare("https://github.com/SEF-Group-25/Launch-Interceptor-Program.git", None)
+    prepare("https://github.com/SEF-Group-25/Launch-Interceptor-Program.git", "main", "9c08d7e", None)
