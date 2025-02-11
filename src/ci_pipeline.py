330d535a305910ef075a6e0fe9130643eaed0865
Tang Shangxuan <bmhtsx@gmail.com>
2025-02-11 15:51:53 +0100

    bug: specify branch and commit when prepare #19
    This modify command to specify branch and commit.

--- src/ci_pipeline.py
+++ src/ci_pipeline.py
@@ -9,13 +9,18 @@ def run_ci_pipeline(repo_url, branch, commit_id, logger):
     build_success = True
 
     try:
-        prepare(repo_url, logger)
+        prepare(repo_url, branch, commit_id, logger)
 
         # check_syntax()
 
         # test()
 
-    except subprocess.CalledProcessError:
+    except subprocess.CalledProcessError as e:
         build_success = False
 
     # notify()
+
+# used for test
+# should be deleted
+if __name__ == "__main__":
+    run_ci_pipeline("https://github.com/SEF-Group-25/Launch-Interceptor-Program.git", "main", "f995103", None)
