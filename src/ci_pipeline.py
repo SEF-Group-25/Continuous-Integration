7cf5e9aa2ae03043a47b063d2aca8c0306779108
Anton Yderberg <yderberg@kth.se>
2025-02-11 16:34:30 +0100

    Feat: Added syntax testing for python #2
    Added syntax checking for python. Could add others but dont know how right now.
    
    Doesnt seem like that is a requirment judging by a question Tang asked

--- src/ci_pipeline.py
+++ src/ci_pipeline.py
@@ -2,7 +2,9 @@
 # This workflow is triggered by webhook
 
 import subprocess
+from config import TMP_DIR
 from src.prepare import prepare
+from src.check_syntax import check_syntax
 
 def run_ci_pipeline(repo_url, branch, commit_id, logger):
 
@@ -11,11 +13,15 @@ def run_ci_pipeline(repo_url, branch, commit_id, logger):
     try:
         prepare(repo_url, logger)
 
-        # check_syntax()
+        check_syntax(logger, TMP_DIR)
 
         # test()
 
     except subprocess.CalledProcessError:
         build_success = False
+    except Exception as e:
+        build_success = False
+        logger.error(f"Build error: {e}")
+
 
     # notify()
